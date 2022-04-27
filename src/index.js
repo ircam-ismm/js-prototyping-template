57367d35b3d23da429568f627821ae249ac8e7a9
b-ma <benjamin.matuszewski@gmail.com>
2022-04-27 18:36:19 +0200

    updated to lit@2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import { render, html } from 'lit-html';
+import { render, html } from 'lit/html.js';
 import { resumeAudioContext } from '@ircam/resume-audio-context';
-// import '@ircam/simple-components/sc-toggle.js';
+
+import '@ircam/simple-components/sc-bang.js';
 
 console.info('> self.crossOriginIsolated', self.crossOriginIsolated);
 
@@ -14,6 +15,14 @@ const audioContext = new AudioContext();
 
   render(html`
     <h2>js-prototyping-template</h2>
+    <sc-bang
+      @input="${e => {
+        const osc = audioContext.createOscillator();
+        osc.connect(audioContext.destination);
+        osc.frequency.value = 200;
+        osc.start();
+        osc.stop(audioContext.currentTime + 0.05);
+      }}"
+    ></sc-bang>
   `, document.body);
 }());
-
